ee337e3974025da9ff14e5b36abfd394060b2254
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-09-16 21:57:33 +0700

    Testing out basic Tailwind CSS Example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-	return <div></div>;
-}
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+import "./App.css";
+
+const App = () => {
+	return (
+		<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+			<div className="flex-shrink-0">
+				<img
+					className="h-12 w-12"
+					src="./favicon.ico"
+					alt="React.js Logo"
+				></img>
+			</div>
+			<div>
+				<div className="text-xl font-medium text-black">React</div>
+				<p className="text-gray-500">You have a new application!</p>
+			</div>
+		</div>
+	);
+};
 
 export default App;
