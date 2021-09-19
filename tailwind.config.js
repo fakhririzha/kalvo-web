52213cfe5dd0e4ed1198b1a1d4f306b011b2df3c
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-09-19 09:02:46 +0000

    Change TailwindCSS into JIT mode (experimental)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 // tailwind.config.js
 module.exports = {
-	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+    mode: "jit",
+    purge: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./public/index.html",
+        "./public/**/*.html",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
